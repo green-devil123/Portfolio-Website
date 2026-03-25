756522acbedc5ab2dc58745fa01f375c9889b459
green-devil123 <agarwal.tarun90@gmail.com>
2026-03-25 22:10:19 +0530

    handle stack

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -71,12 +71,12 @@ const Work = () => {
     tools: "React, Redux, Nodejs, Expressjs, MySQL, MongoDB,HTML,CSS,Javascript, Redis,Typescript, Javascript, Nestjs, Docker, Kubernetes",
     image: "/images/zintellix.webp",
   },
-  {
-    name: "Zintellix",
-    category: "AI-powered e-commerce analytics platform",
-    tools: "React, Redux, Nodejs, Expressjs, MySQL, MongoDB,HTML,CSS,Javascript, Redis,Typescript, Javascript, Nestjs, Docker, Kubernetes",
-    image: "/images/zintellix.webp",
-  }
+    // {
+    //   name: "Zintellix",
+    //   category: "AI-powered e-commerce analytics platform",
+    //   tools: "React, Redux, Nodejs, Expressjs, MySQL, MongoDB,HTML,CSS,Javascript, Redis,Typescript, Javascript, Nestjs, Docker, Kubernetes",
+    //   image: "/images/zintellix.webp",
+    // }
   ]
   return (
     <div className="work-section" id="work">
